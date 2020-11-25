05aacb69b6fd4d274a36484e4d091224fe7422c3
Johan <johandumouchel@gmail.com>
2020-11-26 00:28:53 +0100

    Add Jobs data and CoreCVItems component for test

--- src/CoreCV.js
+++ src/CoreCV.js
@@ -1,17 +1,142 @@
 import React, { Component } from 'react';
 
-class CoreCV extends Component {
-  
+class CoreCVITem extends Component {
   constructor(props){
     super(props);
+  }
 
+  render(){
+    const Txt = this.props.actif ? 'Actif' : 'Pas Actif';
+    const actif = this.props.actif ? 'actif' : '';
+    return(
+      <div className={actif}>
+        <h1>
+          {this.props.coreTxt.title}
+        </h1>
+        <p>{Txt}</p>
+        <div>
+          {}
+        </div>        
+        <button onClick={this.props.onClick}>
+          TEST
+        </button>
+      </div>
+    );
+  }
+}
+
+class CoreCV extends Component {  
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      activeSection:null,
+    }
+  }
+
+  handleClick(section){
+    let activeSection = this.state.activeSection === section ? null : section;
+    this.setState({
+      activeSection: activeSection,
+    });
   }
 
   render(){
     return(
-      <div className="container"> CoreCV </div>
+      <div className="container core-cv">
+        <CoreCVITem 
+          actif={(this.state.activeSection === 'job')} 
+          coreTxt={{title:'Expériences Pro.',jobs:Jobs}}
+          onClick={() => {this.handleClick('job')}}/>
+        <CoreCVITem 
+          actif={(this.state.activeSection === 'skill')} 
+          coreTxt={{title:'Compétences'}}
+          onClick={() => {this.handleClick('skill')}}/>
+        <CoreCVITem 
+          actif={(this.state.activeSection === 'degree')} 
+          coreTxt={{title:'Diplômes'}}
+          onClick={() => {this.handleClick('degree')}}/>
+      </div>
     );
   }
 }
+
+const Jobs = [
+  {
+    title : "Développeur Full Stack - IN'LI - 11/17 - 11/19",
+    skills : [
+      "Conception et développement de solutions applicatives pour de la gestion interne.",
+      "Développement d'outils d'administration",
+      "Mise en production d'application sous divers environnement.",
+      "Maintenance et évolution d'un parc applicatif de gestion interne.",
+      "Gestion de projet sous la méthode SCRUM.",
+    ],
+    env : [
+      "Windows","Azure","SLQ Server","Visual Studio","AzureDevOps","CentOS","Git",
+    ],
+    langage : [
+      "C#","Javascript","php 7","Css","Html","PowerShell","Transact-SQL",
+    ],
+    framework : [
+      "ASP.Net MVC","ASP.Net Web Application","Dynamics CRM","JQuery","FullCalendar JS","Bootstrap",
+    ],
+    degree : null,
+  },
+  {
+    title : "Développeur Full Stack - Paris Habitat - 11/15 - 11/17",
+    skills : [
+      "Conception et développement d'applications web de gestion interne sous Dynamic CRM.",
+      "Création de plugin C# pour ce même progiciel.",
+      "Gestion de projet sous la méthode SCRUM.",
+    ],
+    env : [
+      "Windows","Dynamics CRM","Visual Studio","Git",
+    ],
+    langage : [
+      "C#","Javascript","Css","Html","PowerShell","Transact-SQL",
+    ],
+    framework : [
+      "Dynamics CRM","JQuery",
+    ],
+    degree : "Expert en système d’information - ESMTI Next Formation, Paris - 2017",
+  },
+  {
+    title : "Développeur Back-end - Sotravo - 11/14 - 10/15",
+    skills : [
+      "Conception d'API",
+      "Refonte d’applications internes",
+      "Maintenance et évolution d’API REST",
+      "Gestion de projet sous la méthode Agile (inspiré de SCRUM)",
+    ],
+    env : [
+      "Windows","CentOS","Debian","Git","Sublime Text","NetBean",
+    ],
+    langage : [
+      "Php 5","Html","Css","Less","SQL",
+    ],
+    framework : [
+      "Symfony 3","JQuery","Bootstrap",
+    ],
+    degree : "Concepteur développeur de système d’information - ESMTI Next Formation, Paris - 2015",
+  },
+  {
+    title : "Développeur junior - Le conseil informatique - 10/12 - 10/13",
+    skills : [ 
+      "Conception et développement d'applications avec WinDev 14.",
+      "Rédaction de cahier des charges.",
+      "Intégration Web",
+    ],
+    env : [
+      "Windows","WinDev 14","SAGE Compta", "SAGE Bâtigest",
+    ],
+    langage : [
+      "Html","Css","Access","WLangage",
+    ],
+    framework : [
+      "JQuery", "WinDev 14",
+    ],
+    degree : "BTS SIO - ISIO, Paris - 2014",
+  }
+]
 
 export default CoreCV ;
